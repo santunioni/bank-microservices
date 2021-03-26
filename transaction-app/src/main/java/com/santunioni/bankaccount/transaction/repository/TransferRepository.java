d29dfebcfac3084836460fbe224e9321dea126be
Vinícius Vargas <60983144+santunioni@users.noreply.github.com>
2021-03-25 21:48:48 -0300

    feat(transactions): created the TransferRepository

---
+++ transaction-app/src/main/java/com/santunioni/bankaccount/transaction/repository/TransferRepository.java
@@ -0,0 +1,69 @@
+package com.santunioni.bankaccount.transaction.repository;
+
+import com.santunioni.bankaccount.transaction.domain.Transfer;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+@ApplicationScoped
+public class TransferRepository {
+
+    @Inject
+    EntityManager entityManager;
+
+    @Transactional(REQUIRED)
+    public Transfer save(final Transfer transfer) {
+        entityManager.persist(transfer);
+        return transfer;
+    }
+
+    @Transactional(SUPPORTS)
+    public List<Transfer> findAll() {
+        return entityManager
+                .createQuery("SELECT m FROM Transfer m", Transfer.class)
+                .getResultList();
+    }
+
+    //TODO: implement findById() method
+//    @Transactional(SUPPORTS)
+//    public Transfer findById(final UUID id) throws TransactionNotFoundException {
+//        var optionalTransaction = Optional.ofNullable(
+//                entityManager.find(Transfer.class, id));
+//        if (optionalTransaction.isEmpty()) {
+//            throw new TransactionNotFoundException(id);
+//        }
+//        return optionalTransaction.get();
+
+//    }
+
+
+    // TODO: implement findByAccount() method, which requires
+    //  redesign the arch for Accounts database
+//    @Transactional(SUPPORTS)
+//    public List<Transfer> findByAccount() {
+//        return new ArrayList<Transfer>();
+//    }
+
+    //TODO: implement update() method
+//    @Transactional(REQUIRED)
+//    public Transfer update(final Transfer transfer) {
+//        return entityManager.merge(transfer);
+//    }
+
+    //TODO: implement deleteById() method
+//    @Transactional(REQUIRED)
+//    public void deleteById(final UUID id) throws TransactionNotFoundException {
+//        var transactionFound = Optional.ofNullable(
+//                findById(id)
+//        );
+//        if(transactionFound.isPresent()) {
+//            entityManager.remove(transactionFound);
+//        }
+//    }
+}
