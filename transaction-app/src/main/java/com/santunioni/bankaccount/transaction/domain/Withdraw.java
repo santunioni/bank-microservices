74f064c056256ad439d6212312bf8bed5cfbee9e
Vinícius Vargas <60983144+santunioni@users.noreply.github.com>
2021-03-25 21:47:27 -0300

    feat(transactions): created the Withdraw class

---
+++ transaction-app/src/main/java/com/santunioni/bankaccount/transaction/domain/Withdraw.java
@@ -0,0 +1,29 @@
+package com.santunioni.bankaccount.transaction.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Withdraw {
+
+    @Id
+    private String uuid;
+    @Column(name = "account_id")
+    private String accountId;
+    @Column(name = "value")
+    private double value;
+
+    public Withdraw(String accountId, double value) {
+        this.accountId = accountId;
+        this.value = value;
+        this.uuid = UUID.randomUUID().toString();
+    }
+}
