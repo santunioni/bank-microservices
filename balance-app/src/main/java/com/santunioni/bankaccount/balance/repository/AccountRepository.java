b6469c4d98246a7ebf1ca25e7f9263b257fb3139
Vinícius Vargas <60983144+santunioni@users.noreply.github.com>
2021-03-25 21:53:49 -0300

    feat(balance): add the repository layer

---
+++ balance-app/src/main/java/com/santunioni/bankaccount/balance/repository/AccountRepository.java
@@ -0,0 +1,80 @@
+package com.santunioni.bankaccount.balance.repository;
+
+import com.santunioni.bankaccount.balance.domain.Account;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+@ApplicationScoped
+public class AccountRepository {
+
+    private final EntityManager entityManager;
+
+    @Inject
+    public AccountRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Transactional(REQUIRED)
+    public Account save(final Account account) {
+        entityManager.persist(account);
+        return account;
+    }
+
+    //    @Transactional(REQUIRED)
+    public void deleteById(final String id) {
+//        entityManager.remove(
+//                this.findById(id)
+//        );
+    }
+
+    @Transactional(SUPPORTS)
+    public List<Account> findAll() {
+        var criteriaQuery =
+                entityManager
+                        .getCriteriaBuilder()
+                        .createQuery(Account.class);
+
+        var accountRoot =
+                criteriaQuery
+                        .from(Account.class);
+
+//        var inClause =
+//                entityManager
+//                .getCriteriaBuilder()
+//                .in(accountRoot.get(Account_.nome))
+//                .value(nomeAluno);
+
+        criteriaQuery
+                .select(accountRoot);
+//                .where(inClause);
+
+        try {
+            return entityManager
+                    .createQuery(criteriaQuery)
+                    .getResultList();
+        } catch (Exception e) {
+            return new ArrayList<Account>();
+        }
+    }
+
+
+    @Transactional(SUPPORTS)
+    public Optional<Account> findById(String id) {
+//        return Optional.ofNullable(
+//                entityManager
+//                .createQuery("SELECT id="+id+" FROM Account"
+//                        , Account.class)
+//                .getSingleResult());
+//    }
+        return Optional.ofNullable(null);
+    }
+}
