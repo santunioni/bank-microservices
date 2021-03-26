cea1060a45832933992ac7025980a6eecae578b6
Vinícius Vargas <60983144+santunioni@users.noreply.github.com>
2021-03-25 21:53:37 -0300

    feat(balance): add the resource layer

---
+++ balance-app/src/main/java/com/santunioni/bankaccount/balance/resource/AccountResource.java
@@ -0,0 +1,84 @@
+package com.santunioni.bankaccount.balance.resource;
+
+import com.santunioni.bankaccount.balance.domain.Account;
+import com.santunioni.bankaccount.balance.service.AccountService;
+import org.hibernate.exception.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.status;
+
+@ApplicationScoped
+@Path("/accounts")
+public class AccountResource {
+
+    private final Logger log = LoggerFactory.getLogger(AccountResource.class);
+    private final AccountService accountService;
+
+    @Inject
+    public AccountResource(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Account account) {
+        log.debug("Creating the account " + account);
+        try {
+            account = accountService.create(account);
+        } catch (Exception e)
+        {
+            System.out.println("Account already exist.");
+        }
+        return Response
+                .created(URI.create(account.getAccountID().replaceAll(" ","_")))
+                .build();
+    }
+
+
+//    @DELETE
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response delete(Account account) {
+//        log.debug("Deleting the account " + account);
+//        accountService.delete(account);
+//        return Response.noContent().build();
+//    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response list() {
+        log.debug("List accounts...");
+        return ofNullable(accountService.listAll())
+                .map(Response::ok)
+                .orElse(status(NOT_FOUND))
+                .build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listById(@PathParam("id") final String id) {
+        log.debug("Getting the account " + id);
+//        return ofNullable(accountService.findById(id))
+//                .map(Response::ok)
+//                .orElse(status(NOT_FOUND))
+//                .build();
+        return Response.noContent().build();
+    }
+
+}
